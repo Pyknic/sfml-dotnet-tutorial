01a59f7d2d623f4a3f2061eed5fe8892af2a9a7d
Emil Forslund <emil.forslund@pyknic.se>
2020-01-10 10:57:00 +0100

    Upload first version

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace asteroids_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(RenderWindow window=new RenderWindow(new VideoMode(800,600),"Hello SFML")){
+                window.Closed+=(s,e)=>window.Close();
+                RectangleShape shape=new RectangleShape(new Vector2f(100,100));
+            
+                shape.Position=new Vector2f(400,300);
+                shape.Origin=new Vector2f(50,50);
+                shape.Rotation=45.0f;
+                shape.FillColor=Color.Red;
+
+                while(window.IsOpen){
+                    window.DispatchEvents();
+                    window.Clear();
+                    window.Draw(shape);
+                    window.Display();
+                }
+            } 
+        }
+    }
+}
